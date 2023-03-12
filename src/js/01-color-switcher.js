ef59809e8b290e8cf642b4511ba46769652c6da6
Andrii Veremii <dashuk10@gmail.com>
2023-03-12 22:09:49 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,47 @@
-const
+import '../css/common.css'
+
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyEl: document.body,
+}
+
+const changer = {
+
+    intervalId: null,
+    isActive: false,
+
+    start() {
+
+        if (this.isActive) {
+            return;
+        }
+        this.isActive = true;
+
+        this.intervalId = setInterval(() => {
+             getColorBody();
+        }, 1000, 1000)
+    },
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false; 
+    },
+}
+
+refs.startBtn.addEventListener('click', () => {
+    changer.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+    changer.stop();
+});
+
+const getColorBody = () => {
+    let randomHexColor = getRandomHexColor()
+    refs.bodyEl.style.backgroundColor = randomHexColor;
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
---
+++ src/js/02-timer.js
@@ -0,0 +1,20 @@
+import '../css/common.css'
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    input: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('button[data-start]'),
+    timeEl: document.querySelector('.timer'),
+}
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+    },
+};
