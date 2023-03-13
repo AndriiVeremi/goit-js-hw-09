b08b8a7cf00b8a206cac220067c47cf5893399f5
Andrii Veremii <dashuk10@gmail.com>
2023-03-13 22:43:46 +0200

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,17 @@
 import '../css/common.css';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+const refs = {
+    inputData: document.querySelector('input#datetime-picker'), // input[type="text"]
+    startBtn: document.querySelector('button[data-start]'),
+    timerEl: document.querySelector('.timer'),
+}
+
+let selectedDate = null;
+let currentDate = null;
+refs.startBtn.disabled = true;
 
 const options = {
     enableTime: true,
@@ -9,17 +20,63 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
         console.log(selectedDates[0]);
+        onSetData(selectedDates);
     },
 };
 
-flatpickr("#datetime-picker", options);
+flatpickr("input#datetime-picker", options);
 
-const refs = {
-    input: document.querySelector('input[type="text"]'), //  '#datetime-picker'
-    startBtn: document.querySelector('button[data-start]'),
-    timerEl: document.querySelector('.timer'),
+Report.info(
+  'Хелов май френд!',
+  'Будь добрий тицьни',
+  'Ок!'
+);
+
+function onSetData(selectedDates) {
+    selectedDate = selectedDates[0].getTime();
+    currentDate = new Date().getTime();
+
+    if (selectedDate < currentDate) {
+        refs.startBtn.disabled = true;
+        Report.failure('🥺 Уууу...', 'Братуха, щось не так!');
+    } else {
+        refs.startBtn.disabled = false; 
+        Report.success('Все ок, братанчік!')
+    } 
 }
 
 
 
 
+// function onStartClick(event) {
+    
+
+
+const timer = {
+
+    // intervalId: null,
+    // isActive: false,
+
+    start() {
+        console.log('message')
+        // if (this.isActive) {
+        //     return;
+        // }
+        // this.isActive = true;
+        this.intervalId = setInterval(() => {
+            // getColorBody();
+        }, 1000, 1000)
+    },
+
+    stop() {
+
+        if (this.isActive) {
+            clearInterval(this.intervalId);
+            this.isActive = false;
+        }
+    },
+}
+
+refs.startBtn.addEventListener('click', () => {
+    timer.start();
+})
