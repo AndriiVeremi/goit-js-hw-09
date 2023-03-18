2bbd0e0f4e06114c92d7880b470b477f78e6234e
Andrii Veremii <dashuk10@gmail.com>
2023-03-18 19:36:43 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -42,4 +42,45 @@ function getColorBody() {
     refs.bodyEl.style.backgroundColor = randomHexColor;
 };
 
+ //============== VARIANT 2 =============================================
 
+// const refs = {
+//     startBtn: document.querySelector('button[data-start]'),
+//     stopBtn: document.querySelector('button[data-stop]'),
+//     bodyEl: document.body,
+// }
+
+// const changColorBg = {
+
+//     intervalId: null,
+//     isActive: false,
+
+//     start() {
+
+//         if (this.isActive) {
+//             return;
+//         }
+
+//         this.intervalId = setInterval(() => {
+//             this.isActive = true;
+//             refs.bodyEl.style.backgroundColor = getRandomHexColor();
+//         }, 1000);
+//     },
+
+//     stop() {
+//         clearInterval(this.intervalId);
+//         this.isActive = false;
+//     }
+// }
+
+// refs.startBtn.addEventListener('click', () => {
+//     changColorBg.start();
+// });
+
+// refs.stopBtn.addEventListener('click', () => {
+//     changColorBg.stop();
+// });
+
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// }
